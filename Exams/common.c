6f3f46d32d142ee47258d1424821fd87dda1b82f
LMBao1411 <minhbao14112006@gmail.com>
2026-02-25 20:50:27 -0500

    provided solution for common.c

--- Exams/common.c
+++ Exams/common.c
@@ -12,13 +12,29 @@ void commonChars(char arr[][MAX_LEN], int n) {
   }
 
   for (int i = 0; i < n; i++) {
-    // fill code here
-    for (int i=0; )
+    // file in code here
+    int present[26] = {false};  // intialize temporary record of each string
+    for (int j=0; arr[i][j] != '\0'; j++) { // loop through each character of that string
+      char c = arr[i][j];
+      present[c - 'a'] = true;  // mark the letter of that string true in the present record
+    }
+
+    for (int i=0; i<26; i++) {
+      common[i] = common[i] && present[i];
+    }
   }
 
   printf("Common characters: ");
 
   // fill code here
+  int found = true;
+  for (int i=0; i<26; i++) {
+    if (common[i]==true) {
+      printf("%c ", i + 'a');
+      found = true;
+    }
+  }
+  
   if (!found) {
     printf("None");
   }
