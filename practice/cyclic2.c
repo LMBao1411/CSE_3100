1c52e7efbbc51d7405580696a0f13e381a17da86
LMBao1411 <minhbao14112006@gmail.com>
2026-03-27 14:04:35 -0400

    added two more variations of cyclic

---
+++ practice/cyclic2.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int p_to_c1[2], c1_to_c2[2], c2_to_p[2];
+
+    pipe(p_to_c1);
+    pipe(c1_to_c2);
+    pipe(c2_to_p);
+
+    // Child 1
+    if (fork() == 0) {
+        char ch;
+
+        close(p_to_c1[1]);
+        close(c1_to_c2[0]);
+        close(c2_to_p[0]);
+        close(c2_to_p[1]);
+
+        while (read(p_to_c1[0], &ch, 1) > 0) {
+            printf("child 1 read: %c\n", ch);
+            ch++;
+            printf("child 1 wrote: %c\n", ch);
+            write(c1_to_c2[1], &ch, 1);
+        }
+
+        close(p_to_c1[0]);
+        close(c1_to_c2[1]);
+        exit(0);
+    }
+
+    // Child 2
+    if (fork() == 0) {
+        char ch;
+
+        close(c1_to_c2[1]);
+        close(c2_to_p[0]);
+        close(p_to_c1[0]);
+        close(p_to_c1[1]);
+
+        while (read(c1_to_c2[0], &ch, 1) > 0) {
+            printf("child 2 read: %c\n", ch);
+            ch++;
+            printf("child 2 wrote: %c\n", ch);
+            write(c2_to_p[1], &ch, 1);
+        }
+
+        close(c1_to_c2[0]);
+        close(c2_to_p[1]);
+        exit(0);
+    }
+
+    // Parent
+    char ch = 'a';
+    int count = 0;
+    int MAX = 5;
+
+    close(p_to_c1[0]);
+    close(c1_to_c2[0]);
+    close(c1_to_c2[1]);
+    close(c2_to_p[1]);
+
+    while (count < MAX) {
+        printf("parent wrote: %c\n", ch);
+        write(p_to_c1[1], &ch, 1);
+
+        if (read(c2_to_p[0], &ch, 1) <= 0) break;
+        printf("parent read: %c\n", ch);
+
+        ch++;
+        count++;
+        sleep(1);
+    }
+
+    // 🔑 trigger shutdown
+    close(p_to_c1[1]);
+    close(c2_to_p[0]);
+
+    wait(NULL);
+    wait(NULL);
+
+    return 0;
+}
