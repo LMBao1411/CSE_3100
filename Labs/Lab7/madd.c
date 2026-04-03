fe0585cfdce7fbc0ddb73129d0cc4adda7d8fd51
LMBao1411 <minhbao14112006@gmail.com>
2026-04-03 13:57:59 -0400

    added solution to lab7

--- Labs/Lab7/madd.c
+++ Labs/Lab7/madd.c
@@ -3,62 +3,89 @@
 #include <pthread.h>
 #include "matrix.h"
 
-#define     NUM_THREADS     2
+#define NUM_THREADS 2
 
 typedef struct {
     unsigned int id;
-    TMatrix *m, *n, *t;
+    TMatrix *m;
+    TMatrix *n;
+    TMatrix *t;
 } thread_arg_t;
 
-/* the main function of threads */
-static void * thread_main(void * p_arg)
+/* Main thread:
+    create thread 0
+    create thread 1
+
+Thread 0:          Thread 1:
+    rows 0–2           rows 3–5
+    compute            compute
+
+Main thread:
+    waits (join)
+    waits (join)
+    return result
+*/
+static void *thread_main(void *p_arg)
 {
-    // TODO
-    thread_arg_t *arg = (thread_arg_t *) p_arg;
+    thread_arg_t *arg = (thread_arg_t *)p_arg;
     TMatrix *m = arg->m;
     TMatrix *n = arg->n;
     TMatrix *t = arg->t;
 
-    for (unsigned int i = 0; i < m->nrows; i++) {
-        for (unsigned int j = 0; j < m->ncols; j++) {
+    int start, end;
+    if (arg->id == 0) {
+        start = 0;
+        end = m->nrows / 2;
+    } else {
+        start = m->nrows / 2;
+        end = m->nrows;
+    }
+
+    // compute assigned rows
+    for (int i = start; i < end; i++) {
+        for (int j = 0; j < m->ncols; j++) {
             t->data[i][j] = m->data[i][j] + n->data[i][j];
         }
     }
     return NULL;
 }
 
-/* Return the sum of two matrices. The result is in a newly created matrix. 
- *
- * If a pthread function fails, report error and exit. 
- * Return NULL if something else is wrong.
- *
- * Similar to addMatrix, but this function uses 2 threads.
- */
-TMatrix * addMatrix_thread(TMatrix *m, TMatrix *n)
+/* threaded matrix addition */
+TMatrix *addMatrix_thread(TMatrix *m, TMatrix *n)
 {
-    if (    m == NULL || n == NULL
-         || m->nrows != n->nrows || m->ncols != n->ncols )
+    if (m == NULL || n == NULL)
         return NULL;
 
-    TMatrix * t = newMatrix(m->nrows, m->ncols);
+    if (m->nrows != n->nrows || m->ncols != n->ncols)
+        return NULL;
+
+    TMatrix *t = newMatrix(m->nrows, m->ncols);
     if (t == NULL)
-        return t;
+        return NULL;
 
-    // TODO
     pthread_t threads[NUM_THREADS];
-    thread_arg_t thread_args[NUM_THREADS];
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_args[i].id = i;
-        thread_args[i].m = m;
-        thread_args[i].n = n;
-        thread_args[i].t = t;
+    thread_arg_t args[NUM_THREADS];
 
-        pthread_create(&threads[i], NULL, thread_main, thread_args+i);
+    // create threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        args[i].id = i;
+        args[i].m = m;
+        args[i].n = n;
+        args[i].t = t;
+
+        if (pthread_create(&threads[i], NULL, thread_main, &args[i]) != 0) {
+            perror("pthread_create failed");
+            exit(1);
+        }
     }
 
+    // wait for threads to finish
     for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("pthread_join failed");
+            exit(1);
+        }
     }
-    
+
     return t;
 }
