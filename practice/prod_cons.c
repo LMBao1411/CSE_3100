bbf1e733658bfc47a5bdddf68346c35f3cdc74e1
LMBao1411 <minhbao14112006@gmail.com>
2026-04-09 23:02:30 -0400

    revision producer consumer problme

--- practice/prod_cons.c
+++ practice/prod_cons.c
@@ -36,51 +36,54 @@ void cleanup_sync_objects() {
 }
 
 void *producer_func(void *arg) {
-    int items[4];
-    
     while (1) {
-        for (int i=0; i<4; i++) {
+        // randomly produce between 2 and 5 items
+        int n = rand() % 4 + 2;
+        int items[6];
+
+        for (int i = 0; i < n; i++) {
             items[i] = rand() % 100;
         }
+
         pthread_mutex_lock(&mutex);
 
-        while (count + 4 > BUFFERSIZE) {
+        while (count + n > BUFFERSIZE) {
             printf("producer enters waiting while loop\n");
             pthread_cond_wait(&cond_producer, &mutex);
+            printf("producer woke up\n");
         }
 
-        for (int i=0; i<4; i++) {
+        for (int i = 0; i < n; i++) {
             buffer[count++] = items[i];
             printf("Produced: %d\n", items[i]);
         }
 
         pthread_cond_signal(&cond_consumer);
         pthread_mutex_unlock(&mutex);
-        sleep((rand()%5));
+        sleep(rand() % 4);
     }
 }
 
 void *consumer_func(void *arg) {
-    int items[2];
-
     while (1) {
+        int n = rand() % 4 + 2;
+
         pthread_mutex_lock(&mutex);
 
-        while (count < 2) {
+        while (count < n) {
             printf("consumer enters waiting while loop\n");
             pthread_cond_wait(&cond_consumer, &mutex);
-            printf("signal\n");
-            
+            printf("consumer woke up\n");
         }
 
-        for (int i=0; i < 2; i++) {
-            items[i] = buffer[count--];
-            printf("Consumed: %d\n", items[i]);
+        for (int i = 0; i < n; i++) {
+            int item = buffer[count--];
+            printf("Consumed: %d\n", item);
         }
 
         pthread_cond_signal(&cond_producer);
         pthread_mutex_unlock(&mutex);
-        sleep(rand()%3);
+        sleep(rand() % 3);
     }
 }
 
